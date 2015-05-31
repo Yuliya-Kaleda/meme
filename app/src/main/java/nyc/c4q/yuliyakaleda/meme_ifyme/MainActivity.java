cb5b122121733915d5585c987736e90b88eb0227
Yuliya-Kaleda <yulia.koleda1988@gmail.com>
2015-05-31 16:10:59 -0400

    add second activity

--- app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/MainActivity.java
+++ app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/MainActivity.java
@@ -1,76 +1,73 @@
 package nyc.c4q.yuliyakaleda.meme_ifyme;
 
 import android.content.Intent;
-import android.provider.MediaStore;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int SELECT_PICTURE = 1;
+    private static final int CREATE_PICTURE = 2;
 
     Button take;
     Button choose;
+    Button blackFrame;
+    ImageView image;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        take = (Button) findViewById(R.id.take);
-        take.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
         choose = (Button) findViewById(R.id.choose);
+        take = ...
 
-        choose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        if(choose!=null) {
 
-            }
-        });
+            choose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Log.d("MainActivity", "pressed");
 
 
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    Intent chooser = Intent.createChooser(intent, "Select Picture");
+                    startActivityForResult(chooser, SELECT_PICTURE);
 
-        dispatchTakePictureIntent();
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            });
         }
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case SELECT_PICTURE:
+                Log.d("MainActivity", "I just selected a picture");
+                if (resultCode == RESULT_OK) {
+                    imageUri = data.getData();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                    Intent sentTo = new Intent(MainActivity.this, SecondActivity.class);
+                    sentTo.putExtra("uri", imageUri);
+                    startActivity(sentTo);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                }
+
+            case CREATE_PICTURE:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
