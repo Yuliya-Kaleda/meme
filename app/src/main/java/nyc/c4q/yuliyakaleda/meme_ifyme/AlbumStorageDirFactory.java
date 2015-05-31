3dc6685bfe9b6804963a0796de9da4aafdba9827
Amcbri15 <anthony.mcbride0711@gmail.com>
2015-05-31 16:49:07 -0400

    Added Album and Jpeg-to-Bitmap code

---
+++ app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/AlbumStorageDirFactory.java
@@ -0,0 +1,7 @@
+package nyc.c4q.yuliyakaleda.meme_ifyme;
+
+import java.io.File;
+
+abstract class AlbumStorageDirFactory {
+    public abstract File getAlbumStorageDir(String album);
+}
--- app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/MainActivity.java
+++ app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/MainActivity.java
@@ -1,38 +1,141 @@
 package nyc.c4q.yuliyakaleda.meme_ifyme;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+    private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewVisibility";
 
-    Button take;
-    Button choose;
+    private ImageView mImageView;
+    private Bitmap mImageBitmap;
+
+    private String mCurrentPhotoPath;
+
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+
+    private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
+
+    Button mTakeButton;
+    Button mChooseButton;
+
+    // Photo album for this app
+    private String getAlbumNAme() {
+        return getString(R.string.album_name);
+    }
+    private File getAlbumDir(){
+        File storageDir = null;
+
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumNAme());
+
+            if (storageDir != null){
+                if(! storageDir.mkdirs()){
+                    if (! storageDir.exists()) {
+                        Log.d("CameraSample", "failed to create directory");
+                    }
+                }
+            }
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+
+        return storageDir;
+    }
+
+    // Create an image file name
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+        File albumF = getAlbumDir();
+        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+        return imageF;
+    }
+
+    private File setUpPhotoFile() throws IOException {
+        File f = createImageFile();
+        mCurrentPhotoPath = f.getAbsolutePath();
+
+        return f;
+    }
+
+    private void setPic(){
+
+        // Gets the size of the ImageView
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the size of the image
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Calculate scaling needs
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+        // Set bitmap options to scale the image decode target
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        // Decode the JPEG file into a Bitmap
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+        // Set bitmap to ImageView
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setVisibility(View.VISIBLE);
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        take = (Button) findViewById(R.id.take);
-        take.setOnClickListener(new View.OnClickListener() {
+        mTakeButton = (Button) findViewById(R.id.take);
+        mTakeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        choose = (Button) findViewById(R.id.choose);
+        mChooseButton = (Button) findViewById(R.id.choose);
 
-        choose.setOnClickListener(new View.OnClickListener() {
+        mChooseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
