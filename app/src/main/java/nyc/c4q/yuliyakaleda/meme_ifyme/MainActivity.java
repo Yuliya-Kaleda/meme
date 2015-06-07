9b7f1d323f85ffc6a73b3538809b9cda243c851e
Yuliya-Kaleda <yulia.koleda1988@gmail.com>
2015-06-07 08:23:17 -0400

    initial

--- app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/MainActivity.java
+++ app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/MainActivity.java
@@ -1,86 +1,54 @@
 package nyc.c4q.yuliyakaleda.meme_ifyme;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 public class MainActivity extends Activity {
 
     private static final int SELECT_PICTURE = 1;
     private static final int REQUEST_IMAGE_CAPTURE = 2;
-    public static final String TAG = "MEME_ACTIVITY";
-    Button take;
-    Button choose;
-    Button blackFrame;
-    ImageView image;
-    Uri imageUri;
-    Bitmap bitmap;
+
+    private Button take;
+    private Button choose;
+    private Uri imageUri;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         take = (Button) findViewById(R.id.take);
-        take.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent taker = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (taker.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(taker, REQUEST_IMAGE_CAPTURE);
-                }
-            }
-        });
         choose = (Button) findViewById(R.id.choose);
-        choose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "pressed");
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                Intent chooser = Intent.createChooser(intent, "Select Picture");
-                startActivityForResult(chooser, SELECT_PICTURE);
-            }
-        });
+
+        setEventListener(true);
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, String.format("onActivityResult(): SELECT_PICTURE==%s, REQUEST_IMAGE_CAPTURE==%s, resultCode==%s",
-                requestCode == SELECT_PICTURE,
-                requestCode == REQUEST_IMAGE_CAPTURE,
-                resultCode == RESULT_OK));
+
         Bitmap imageBitmap = null;
         switch (requestCode) {
             case SELECT_PICTURE:
-                Log.d(TAG, "onActivityResult(): I just selected a picture");
                 if (resultCode == RESULT_OK) {
                     imageUri = data.getData();
                     try {
                         imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-// http://stackoverflow.com/a/10281667, image is too big so you gotta resize
+                        // http://stackoverflow.com/a/10281667, image is too big so you gotta resize
                         imageBitmap = Bitmap.createScaledBitmap(imageBitmap, 200, 200, true);
                     } catch (IOException e) {
-                        Log.d(TAG, e.getMessage());
                     }
                 }
                 break;
+
             case REQUEST_IMAGE_CAPTURE:
-                Log.d(TAG, "onActivityResult(): I just took a picture");
                 if (resultCode == RESULT_OK) {
                     Bundle extras = data.getExtras();
                     imageBitmap = (Bitmap) extras.get("data");
@@ -91,4 +59,32 @@ public class MainActivity extends Activity {
         sentTo.putExtra("bitmap", imageBitmap);
         startActivity(sentTo);
     }
-}
+
+    public void setEventListener(boolean setFlag) {
+        if (!setFlag) {
+            take.setOnClickListener(null);
+            choose.setOnClickListener(null);
+        }
+        else {
+            take.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent taker = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (taker.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(taker, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+            });
+            choose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    Intent chooser = Intent.createChooser(intent, "Select Picture");
+                    startActivityForResult(chooser, SELECT_PICTURE);
+                }
+            });
+        }
+    }
+ }
--- app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/SecondActivity.java
+++ app/src/main/java/nyc/c4q/yuliyakaleda/meme_ifyme/SecondActivity.java
@@ -1,185 +1,145 @@
 package nyc.c4q.yuliyakaleda.meme_ifyme;
 
 import android.app.Activity;
-import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.DragEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 /**
  * Created by July on 5/31/15.
  */
 public class SecondActivity extends Activity {
-    private static final String TAG = SecondActivity.class.getSimpleName();
 
-    public static final String MY_FILE = "photo";
-    ImageView image;
-    ViewGroup tv;
-    Button share;
-    Button motivation;
-    Button vanilla;
-    Uri myUri;
-    Bitmap bm;
-    EditText text1;
-    EditText text2;
-    EditText text3;
-    ImageView imageBackground;
-
+    private ImageView image;
+    private Button share;
+    private Button demotivation;
+    private Button vanilla;
+    private Bitmap bm;
+    private Bitmap modifiedBit;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(MainActivity.TAG, "SecondActivity.onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image);
 
-        motivation = (Button) findViewById(R.id.motivation);
-        motivation.setOnClickListener(new View.OnClickListener() {
-                  @Override
-                       public void onClick(View view) {
-                         Intent intent = new Intent();
-                         imageBackground.setVisibility(View.VISIBLE);
-                                          }
-                                      });
+        initializeViews();
+        getIntentInfo();
+        setEventListener(true);
+    }
 
+    public void initializeViews() {
+        image = (ImageView) findViewById(R.id.image1);
+        demotivation = (Button) findViewById(R.id.demotivation);
         vanilla = (Button) findViewById(R.id.vanilla);
-        vanilla.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                imageBackground.setVisibility(View.INVISIBLE);
-            }
-        });
+        share = (Button) findViewById(R.id.share_button);
+    }
 
-
-
-        text1 =(EditText) findViewById(R.id.addText1);
-        text2 =(EditText) findViewById(R.id.addText2);
-        text3 =(EditText) findViewById(R.id.addText3);
-
-
-       image = (ImageView) findViewById(R.id.image1);
-       //tv = (RelativeLayout) findViewById(R.id.merge_image);
-       share = (Button) findViewById(R.id.share_button);
-       //get the uri from the intent sent from MainActivity
+    public void getIntentInfo() {
         Intent intent = getIntent();
-        Log.d(MainActivity.TAG, String.format("SecondActivity.onCreate() intent:", intent));
         bm = intent.getParcelableExtra("bitmap");
+        image.setImageBitmap(bm);
+
         int height = bm.getHeight();
         int width = bm.getWidth();
         bm = Bitmap.createScaledBitmap(bm, height, width, false);
-        image.setImageBitmap(bm);
+    }
 
+    public void setEventListener(boolean setFlag) {
+        if(!setFlag) {
+            share.setOnClickListener(null);
+            demotivation.setOnClickListener(null);
+            vanilla.setOnClickListener(null);
+        }
+        else {
 
-     // make a screen shot of our layout to convert into an image to share
-//        View u = findViewById(R.id.scroll);
-//        u.setDrawingCacheEnabled(true);
-//        ScrollView z = (ScrollView) findViewById(R.id.screen);
-//        int totalHeight = z.getChildAt(0).getHeight();
-//        int totalWidth = z.getChildAt(0).getWidth();
-//        u.layout(0, 0, totalWidth, totalHeight);
-//        u.buildDrawingCache(true);
-//        Bitmap b = Bitmap.createBitmap(u.getDrawingCache());
-//        u.setDrawingCacheEnabled(false);
+            demotivation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setContentView(R.layout.demotivational);
 
+                   // RelativeLayout rl = (RelativeLayout) findViewById(R.id.rl);
+                    ImageView imBlackBack = (ImageView) findViewById(R.id.image_black_back);
+                    imBlackBack.setImageBitmap(bm);
 //
-//        //Save bitmap
-//        String extr = Environment.getExternalStorageDirectory().toString() +   File.separator + "Folder";
-//        String fileName = new SimpleDateFormat("yyyyMMddhhmm'_report.jpg'").format(new Date());
-//        File myPath = new File(extr, fileName);
-//        FileOutputStream fos = null;
-//        try {
-//            fos = new FileOutputStream(myPath);
-//            b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-//            fos.flush();
-//            fos.close();
-//            MediaStore.Images.Media.insertImage(getContentResolver(), b, "Screen", "screen");
-//        }catch (FileNotFoundException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
+//                    rl.setDrawingCacheEnabled(true);
+//                    rl.buildDrawingCache();
+//                    modifiedBit = rl.getDrawingCache();
+                    share = (Button) findViewById(R.id.share_button);
+                    setEventListener(true);
 
-//        String path = Environment.getExternalStorageDirectory().toString();
-//        OutputStream fOut = null;
-//        File file = new File(path, "FitnessGirl.jpg"); // the File to save to
-//        try {
-//            fOut = new FileOutputStream(file);
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        b.compress(Bitmap.CompressFormat.JPEG, 85, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
-//        try {
-//            fOut.flush();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            fOut.close(); // do not forget to close the stream
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            MediaStore.Images.Media.insertImage(getContentResolver(),file.getAbsolutePath(),file.getName(),file.getName());
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-
-
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String filename = "sharedImage";
-                try {
-                    File tempFile = File.createTempFile(filename, "jpg");
-                    FileOutputStream fos = new FileOutputStream(tempFile);
-                    bm.compress(Bitmap.CompressFormat.JPEG,100, fos);
-                    shareVia(tempFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            }
-        });
+            });
+            vanilla.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setContentView(R.layout.vanilla);
 
+                    ImageView imBlackBack = (ImageView) findViewById(R.id.image_black_back);
+                    imBlackBack.setImageBitmap(bm);
 
+                    share = (Button) findViewById(R.id.share_button);
+                    setEventListener(true);
+
+                }
+            });
+            share.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    shareVia(bm);
+                }
+            });
+        }
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
     //method to share an image via social networks
-    public void shareVia(File attachment) {
-        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-        sharingIntent.setType("image/jpeg");
-        sharingIntent.putExtra(Intent.EXTRA_STREAM, attachment.toURI());
-       // startActivity(Intent.createChooser(se(getResources((R.id.merge_image));
-        startActivity(Intent.createChooser(sharingIntent, "Share image using"));
+        public void shareVia(Bitmap mBitmap) {
+            File f;
+            Intent share = new Intent(Intent.ACTION_SEND);
+            share.setType("image/jpeg");
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            try {
+                f = createImageFile();
+                FileOutputStream fo = new FileOutputStream(f);
+                fo.write(bytes.toByteArray());
+                fo.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            share.putExtra(Intent.EXTRA_STREAM, Uri.parse(mCurrentPhotoPath));
+            startActivity(Intent.createChooser(share, "Share Image"));
+        }
     }
-}
+
